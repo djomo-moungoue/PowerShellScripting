d271a2396503689330161bbe96bc9c2a74142fd6
djomo.moungoue <djomo.moungoue@hotmail.com>
2023-11-22 08:03:51 +0100

    Import Users from CSV object

---
+++ ps1_scripts/ImportUsersFromCsvObject.ps1
@@ -0,0 +1,15 @@
+﻿$UsersFromCsvObject = Import-csv -Path "C:\Users\admin\powershell_scripting\users.csv"
+$LocalUsers = Get-LocalUser | Select-Object Name
+$LocalUsers
+foreach($User in $UsersFromCsvObject)
+{
+    if($User.LOGONNAME -notin $LocalUsers)
+    {
+        $User.LOGONNAME
+
+        $SecureString = ConvertTo-SecureString -String $User.PASSWORDOFUSER -AsPlainText -Force
+        New-LocalUser -Name $User.LOGONNAME -Password $SecureString -Description $User.DESCRIPTION
+    }
+}
+Write-Host
+Get-LocalUser
---
+++ ps1_scripts/create_dirs_from_file_input.ps1
@@ -0,0 +1,25 @@
+﻿$Sources = "C:\Users\admin\powershell_scripting\sources"
+if(!(Test-Path $Sources))
+{
+    $Sources = New-Item -Path "C:\Users\admin\powershell_scripting\sources" -ItemType directory
+}
+$Dirs = "$Sources\dirs.txt"
+if(!(Test-Path $Dirs))
+{
+    $Dirs = New-Item -Path "$Sources\dirs.txt" -ItemType file
+}
+Set-Content -Path $Dirs -Value "user01"
+Add-Content -Path $Dirs -Value "user02"
+Add-Content -Path $Dirs -Value "user03"
+Add-Content -Path $Dirs -Value "user04"
+Add-Content -Path $Dirs -Value "user05"
+$Files = Get-Content -Path $Dirs
+
+foreach($File in $Files)
+{
+    if(!(Test-Path "$Sources\$File"))
+    {
+        New-Item -path $Sources -Name $File -ItemType "directory"
+    }
+}
+Get-ChildItem $Sources
