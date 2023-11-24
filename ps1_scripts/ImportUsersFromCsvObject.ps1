40e547bc2a9fd1228edc852ab8ccbd6a33e75d6b
djomo.moungoue <djomo.moungoue@hotmail.com>
2023-11-24 07:38:51 +0100

    Lab 2.5

--- ps1_scripts/ImportUsersFromCsvObject.ps1
+++ ps1_scripts/ImportUsersFromCsvObject.ps1
@@ -1,17 +1,26 @@
-﻿$UsersFromCsvObject = Import-csv -Path "C:\Users\admin\powershell_scripting\users.csv"
-$UsersFromCsvObject.GetType()
-$UsersFromCsvObject | Format-Table
+﻿$CurrentComputer = $env:COMPUTERNAME
+Get-Date -Format "dd/MM/yyyy hh:mm:ss"
+Write-Host
+"==========================$CurrentComputer======================"
+Write-Host
+if($CurrentComputer -eq "DESKTOP-18EEUA3")
+{# Make sure the script is ran on the correct computer
+    $UsersFromCsvObject = Import-csv -Path "C:\Users\admin\powershell_scripting\users.csv"
+    $UsersFromCsvObject | Format-Table
 
-foreach($User in $UsersFromCsvObject)
-{
-    # Count the occurences of local users named $User.LOGONNAME
-    $Count = Get-LocalUser -Name $User.LOGONNAME | Measure-Object
-    $User.LOGONNAME 
-    $Count.Count
-    if($Count.Count -eq 0)
+    foreach($User in $UsersFromCsvObject)
     {
+        # Count the occurences of local users named $User.LOGONNAME
+        $Count = Get-LocalUser -Name $User.LOGONNAME | Measure-Object
+        if($Count.Count -eq 0)
+        {
 
-        $SecurePassword = ConvertTo-SecureString -String $User.PASSWORDOFUSER -AsPlainText -Force
-        New-LocalUser -Name $User.LOGONNAME -Password $SecurePassword -Description $User.DESCRIPTION
+            $SecurePassword = ConvertTo-SecureString -String $User.PASSWORDOFUSER -AsPlainText -Force
+            New-LocalUser -Name $User.LOGONNAME -Password $SecurePassword -Description $User.DESCRIPTION
+        }
     }
 }
+Write-Host
+"==========================$CurrentComputer======================"
+Write-Host
+Get-Date -Format "dd/MM/yyyy hh:mm:ss"
---
+++ ps1_scripts/Lab2-5.ps1
@@ -0,0 +1,12 @@
+﻿<#
+Create a PowerShell script which will import these users locally. 
+- Make sure that these users are able to login. 
+- Also make sure that the department header in the CSV file will be the description property of the new localuser.
+#>
+
+$NewPersonals = Import-Csv -Path "C:\Users\admin\powershell_scripting\new_personal.csv"
+
+foreach($NewPersonal in $NewPersonals)
+{
+    $SecurePassword = ConvertTo-SecureString $NewPersonals.Pa
+}
